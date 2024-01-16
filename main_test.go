d62b8b785d1dd521bed23636efb0a199b93e8802
Franck Leroy <fleroy@isagri.fr>
2024-01-16 17:31:31 +0100

    fix tests asserts in windows

--- main_test.go
+++ main_test.go
@@ -1,23 +1,26 @@
 package main
 
-import "testing"
+import (
+	"path/filepath"
+	"testing"
+)
 
 func Test_DefineFilePath_MultiFileChoice(t *testing.T) {
 	want := "tests/example-1.js"
-	if got, _ := DefineFilePath("./tests", "example"); got != want {
+	if got, _ := DefineFilePath("./tests", "example"); filepath.ToSlash(got) != want {
 		t.Errorf("DefineFilePath() = %v, want %v", got, want)
 	}
 }
 
 func Test_DefineFilePath_OneFileChoice(t *testing.T) {
 	want := "tests/example-2.js"
-	if got, _ := DefineFilePath("./tests", "example-2"); got != want {
+	if got, _ := DefineFilePath("./tests", "example-2"); filepath.ToSlash(got) != want {
 		t.Errorf("DefineFilePath() = %v, want %v", got, want)
 	}
 }
 
 func Test_DefineFilePath_ErrorNoFileFound(t *testing.T) {
-	if _, err := DefineFilePath("./tests", "toto"); err.Error() != "No file found with pattern: tests/toto*.js" {
+	if _, err := DefineFilePath("./tests", "toto"); err.Error() != "No file found with pattern: "+filepath.ToSlash("tests/toto*.js") {
 		t.Errorf("DefineFilePath() does not return an error : " + err.Error())
 	}
 }
