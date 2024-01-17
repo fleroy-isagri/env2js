47a81a1b0263e0bd1c69dc841ce935b72e231e56
Franck Leroy <fleroy@isagri.fr>
2024-01-17 16:46:52 +0100

    improve code & add unit test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"path/filepath"
 	"testing"
 )
@@ -22,5 +23,45 @@ func Test_DefineFilePath_OneFileChoice(t *testing.T) {
 func Test_DefineFilePath_ErrorNoFileFound(t *testing.T) {
 	if _, err := DefineFilePath("./tests", "toto"); filepath.ToSlash(err.Error()) != "No file found with pattern: "+filepath.ToSlash("tests/toto*.js") {
 		t.Errorf("DefineFilePath() does not return an error : " + filepath.ToSlash(err.Error()))
+	}
+}
+
+func Test_parseFlags_Version(t *testing.T) {
+	// Arrange
+	version = "1.0.0"
+	expectedOutput := "version : 1.0.0\ncommit  : \ndate    : \nbuiltBy : \n"
+
+	// Act
+	config, output, _ := ParseFlags("prog", []string{"-version"})
+
+	// Assert
+	if config.version != true {
+		t.Errorf("config.version should be true")
+	}
+	if output != expectedOutput {
+		t.Errorf("output = %v, expected %v", output, expectedOutput)
+	}
+}
+
+func Test_parseFlags_Help(t *testing.T) {
+	// Arrange
+	version = "1.0.0"
+	expectedOutput := "Usage of prog:\n  -version\n    \tDisplay version and exit\n"
+
+	// Act
+	config, output, err := ParseFlags("prog", []string{"-help"})
+
+	// Assert
+	if config != nil {
+		t.Errorf("config should be nil")
+	}
+	if err == nil {
+		t.Errorf("err should not be nil")
+	}
+	if err != flag.ErrHelp {
+		t.Errorf("err should be errorFlagHelp")
+	}
+	if output != expectedOutput {
+		t.Errorf("output = %v, expected %v", output, expectedOutput)
 	}
 }
